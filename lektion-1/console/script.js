744ce94df4cdca60d09eb73af11a1f1c4f2885b5
Jonas Mets <jonas.mets@outlook.com>
2019-11-18 13:45:53 +0100

    lagt till mapp

---
+++ lektion-1/console/script.js
@@ -0,0 +1,18 @@
+// 
+// basic javascript
+//  /*  */
+/*  märkspråk   skript språk   programmeringsspråk
+//   html       javascript      C C++ C# 
+//   css        powershell        python
+//   xml        cmd lets          Java
+                  python
+*/              
+
+
+// document.body.innerText = 'text från javascript';
+
+console.log('hej');
+console.warn('en varning');
+console.error('ett fel');
+
+
