cac488ee618124c1308d6c48a2fe00e269f6eac9
Jonas Mets <jonas.mets@outlook.com>
2019-11-18 15:40:21 +0100

    mer mappar

---
+++ lektion-1/arrays/script.js
@@ -0,0 +1,20 @@
+
+//  Arrays
+
+/* 
+deklaration
+  let array1 = ['text 1', 123, true];
+
+*/
+const array1 = ['text 1','text 2','text 2'];
+
+// man kan blanda typer men ska undvikas
+const array2 = ['text 1', 123, true];
+
+console.log(array1);
+
+console.log(array1[0]);
+console.log(array1[1]);
+console.log(array1[2].length);
+
+
---
+++ lektion-1/datatypes/script.js
@@ -0,0 +1,46 @@
+
+
+//  variabler
+
+/*
+Typer:
+string
+number
+boolean
+null
+undefined
+object
+symbol
+*/
+
+/* 
+Deklarera variabler
+
+var name = '';global variabel använd ej
+
+let name = ''; lokal variabel
+
+const name = ''; konstant kan inte skrivas över
+
+
+strings
+let name = " "; använd ej
+let name = ' '; använd
+let name =  ` `; använd med placeholder ${} `hej mitt namn är ${name}`
+*/
+
+// let name = 'Bob';
+// let withplaceholder = `hej mitt namn är ${name}`;
+
+// console.log(withplaceholder);
+
+
+// Javascript skrivs med "camelCase"
+//  let myName = '';
+
+
+
+
+
+
+
