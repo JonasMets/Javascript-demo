87658e29c917e720519a08fdc38167dc345562cf
Jonas Mets <jonas.mets@outlook.com>
2019-12-02 13:05:58 +0100

    lagt till lite

--- lektion-5/validate-functions/script.js
+++ lektion-5/validate-functions/script.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
 
     // function validateField(input, error_message) {
     //     let value = $(input).val();
@@ -15,10 +15,10 @@ $(function() {
     function validateFirstName() {
         let value = $('#firstName').val()
 
-        if(value === '') {
+        if (value === '') {
             $('#firstName_Error').text('Du måste ange ett förnamn');
-        } 
-        else if(value.length < 2) {
+        }
+        else if (value.length < 2) {
             $('#firstName_Error').text('Du måste minst ange 2 tecken');
         }
         else {
@@ -26,10 +26,25 @@ $(function() {
         }
     }
 
-    $('#oneForm').on('submit', function(e) {
+    $('#oneForm').on('submit', function (e) {
         //förhindra att den skickar datat och laddar om sidan
         e.preventDefault();
         validateFirstName();
     });
+
+
+
+    function ValidateEmail(inputText) {
+        var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if (inputText.value.match(mailformat)) {
+            // document.form1.text1.focus();
+            return true;
+        }
+        else {
+            alert("You have entered an invalid email address!");
+            // document.form1.text1.focus();
+            return false;
+        }
+    }
 
 });
