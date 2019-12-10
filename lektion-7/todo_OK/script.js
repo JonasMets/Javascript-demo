92db09c53c16ec4da6258fda912f92b41d730eb1
Jonas Mets <jonas.mets@outlook.com>
2019-12-10 18:13:56 +0100

    lagt till lite

--- lektion-7/todo_OK/script.js
+++ lektion-7/todo_OK/script.js
@@ -10,7 +10,7 @@ function addTodo(text) {
 
     list.insertAdjacentHTML('afterbegin', `
         <li data-key="${todo.id}">
-            <input id="${todo.id}" type="checkbox">
+            <input id="${todo.id}" type="checkbox">Ok
             <label for"${todo.id}" class="complete">
             <span class="todotext">${todo.text} </span>
             <button id="tododelete" class="delete">X</button>
@@ -22,19 +22,26 @@ function deleteTodo(datakey) {
     todoItems = todoItems.filter(item => item.id !== Number(datakey));
 
     const item = document.querySelector(`[data-key='${datakey}']`);
-
-    // console.log(datakey);
-    // console.log(item);
     item.remove();
 }
   
-function toggleComplete(datakey) {
+function toggleComplete(e,datakey) {
     const index = todoItems.findIndex(item => item.id === Number(datakey));
     todoItems[index].checked = !todoItems[index].checked;
 
-    // console.log(datakey);
-    console.log(todoItems[index].checked);
+    console.log(datakey);
+    
+    // test
+    let checkbox2 = document.getElementById('checkbox2');
+    checkbox2.checked = !checkbox2.checked;
+    // console.log(checkbox2.checked);
+    // test
 
+    // let box = document.getElementById(`${datakey}`);
+    let box=document.querySelector(`[data-key='${datakey}']`);
+    console.log(box.classList);
+    
+    // 
     // const item = document.querySelector(`[data-key]=${datakey}`);
     const item = document.querySelector(`[data-key='${datakey}']`);
     if(todoItems[index].checked) {
@@ -43,18 +50,12 @@ function toggleComplete(datakey) {
     else {
         item.classList.remove('completed');
     }
-}
+};
 
 
 list.addEventListener('click', (e) => {
 
     e.preventDefault();
-    //  .target.id
-    console.log(e);
-    console.log(e.target.className);
-    console.log(e.target.nodeName);
-    console.log(e.srcElement.id);
-    console.log(e.target.type);
 
     // if(e.target.classList.contains('complete'))
     if(e.target.type === 'checkbox') {
@@ -62,17 +63,13 @@ list.addEventListener('click', (e) => {
         // const datakey = e.target.parentElement.previousElementSibling.id;
         const datakey = e.target.id;
         // 
-        console.log(datakey);
-        toggleComplete(datakey);
+        // console.log(datakey);
+        toggleComplete(e,datakey);
     }
 // if(e.target.classList.contains('delete') && e.target.nodeName !== 'SPAN') BUTTON
     if(e.target.nodeName === 'BUTTON' && e.srcElement.id === 'tododelete') {
         const datakey = e.target.parentElement.previousElementSibling.id;
-        // const datakey = e.target.id;
-        // const datakey = e.target.parentElement.id;
-        // const datakey =e.srcElement.id;
 
-        
         console.log(datakey);
         deleteTodo(datakey);
     }
